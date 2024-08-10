936895ce7534e4b40622f1ac5719161a3dc96261
Somya Jha <m2somya@gmail.com>
2024-08-10 22:31:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Newton-sCradleC27
+# Newton-sCradle
+
+> I think this comes under Scientific Programming?
+
+#### Made a primitive simulation of the Newton's Cradle using concepts of game development in p5-play.js
+
+#### Addition: Position vs Time graph for every bob added as well (in sketch.js)
--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -8,76 +7,113 @@ const Constraint = Matter.Constraint;
 var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5, roofObject;
 var rope1, rope2, rope3, rope4, rope5;
 var world;
-
+var positions = [[], [], [], [], []]; // To store positions of the bobs over time
+var time = []; // To store the corresponding time points
 
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+  createCanvas(1600, 700);
+  rectMode(CENTER);
 
+  engine = Engine.create();
+  world = engine.world;
 
-	engine = Engine.create();
-	world = engine.world;
+  roofObject = new roof(350, 90, 280, 20);
 
-	roofObject = new roof(350,90,280,20);
+  bobDiameter = 40;
 
-	bobDiameter=100;
-
-	bobObject1 = new bob(280,420,40);
-	bobObject2 = new bob(320,420,40);
-	bobObject3 = new bob(360,420,40);
-	bobObject4 = new bob(400,420,40);
-	bobObject5 = new bob(440,420,40);
-	roof1 = new roof(380,100,600,30);
-	rope1 = new rope(bobObject1.body,roof1.body,-100,0);
-	rope2 = new rope(bobObject2.body,roof1.body,-60,0);
-	rope3 = new rope(bobObject3.body,roof1.body,-20,0);
-	rope4 = new rope(bobObject4.body,roof1.body,20,0);
-	rope5 = new rope(bobObject5.body,roof1.body,60,0);
-	Engine.run(engine);
-	
+  bobObject1 = new bob(350 - bobDiameter * 2, 400, bobDiameter);
+  bobObject2 = new bob(350 - bobDiameter, 400, bobDiameter);
+  bobObject3 = new bob(350, 400, bobDiameter);
+  bobObject4 = new bob(350 + bobDiameter, 400, bobDiameter);
+  bobObject5 = new bob(350 + bobDiameter * 2, 400, bobDiameter);
   
-}
+  roof1 = new roof(350, 90, 280, 20);
+  rope1 = new rope(bobObject1.body, roof1.body, -bobDiameter * 2, 0);
+  rope2 = new rope(bobObject2.body, roof1.body, -bobDiameter, 0);
+  rope3 = new rope(bobObject3.body, roof1.body, 0, 0);
+  rope4 = new rope(bobObject4.body, roof1.body, bobDiameter, 0);
+  rope5 = new rope(bobObject5.body, roof1.body, bobDiameter * 2, 0);
 
+  Engine.run(engine);
+}
 
 function draw() {
   rectMode(CENTER);
   background(230);
   roofObject.display();
 
-  rope1.display()
-  rope2.display()
-  rope3.display()
-  rope4.display()
-  rope5.display()	
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
   bobObject1.display();
   bobObject2.display();
   bobObject3.display();
   bobObject4.display();
   bobObject5.display();
- 
-  
-  
-	
-  
- 
-  
-  
- 
+
+  // Record positions for the graph
+  positions[0].push(bobObject1.body.position.x);
+  positions[1].push(bobObject2.body.position.x);
+  positions[2].push(bobObject3.body.position.x);
+  positions[3].push(bobObject4.body.position.x);
+  positions[4].push(bobObject5.body.position.x);
+  time.push(frameCount);
+
+  // Display the graph
+  drawGraph();
 }
 
 function keyPressed() {
-  	if (keyCode === UP_ARROW) {
-
-    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
-
-  	}
+  if (keyCode === UP_ARROW) {
+    Matter.Body.applyForce(bobObject1.body, bobObject1.body.position, {x: -50, y: -45});
+  }
 }
 
+// Function to draw the graph
+function drawGraph() {
+  push();
+  translate(800, 100);
+  noFill();
+  strokeWeight(2);
 
+  // Draw time vs. position graph
+  stroke(255, 0, 0);
+  beginShape();
+  for (let i = 0; i < positions[0].length; i++) {
+    vertex(time[i] * 0.1, positions[0][i] * 0.1);
+  }
+  endShape();
 
+  stroke(0, 255, 0);
+  beginShape();
+  for (let i = 0; i < positions[1].length; i++) {
+    vertex(time[i] * 0.1, positions[1][i] * 0.1);
+  }
+  endShape();
 
+  stroke(0, 0, 255);
+  beginShape();
+  for (let i = 0; i < positions[2].length; i++) {
+    vertex(time[i] * 0.1, positions[2][i] * 0.1);
+  }
+  endShape();
 
+  stroke(255, 255, 0);
+  beginShape();
+  for (let i = 0; i < positions[3].length; i++) {
+    vertex(time[i] * 0.1, positions[3][i] * 0.1);
+  }
+  endShape();
 
+  stroke(255, 0, 255);
+  beginShape();
+  for (let i = 0; i < positions[4].length; i++) {
+    vertex(time[i] * 0.1, positions[4][i] * 0.1);
+  }
+  endShape();
 
-
-
+  pop();
+}
